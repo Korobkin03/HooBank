e4da4c8aab51e04b61b5d674160a8564fa3ca2a7
Korobkin03 <maks.korobkin.03@mail.ru>
2023-03-12 10:15:28 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App: React.FC = () => (
+  <div className="bg-primary w-full overflow-hidden"></div>
+);
+
+export default App;
---
+++ src/constants/types.ts
@@ -0,0 +1,44 @@
+export type NavLinksType = {
+  id: string;
+  title: string;
+};
+
+export type FeaturesType = {
+  id: string;
+  icon: string;
+  title: string;
+  content: string;
+};
+
+export type FeedbackType = {
+  id: string;
+  content: string;
+  name: string;
+  title: string;
+  img: string;
+};
+
+export type StatsType = {
+  id: string;
+  title: string;
+  value: string;
+};
+
+export type FooterLinksType = {
+  title: string;
+  links: {
+    name: string;
+    link: string;
+  }[];
+};
+
+export type SocialMediaType = {
+  id: string;
+  icon: string;
+  link: string;
+};
+
+export type ClientsType = {
+  id: string;
+  logo: string;
+};
